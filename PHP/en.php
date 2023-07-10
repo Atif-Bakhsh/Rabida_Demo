b5e5ff05eac7851c9597b3a649049c6f23380f0a
atif-bakhsh <atif.bakhsh@gmail.com>
2023-07-10 15:27:41 +0300

    Added new .Strings file

---
+++ PHP/en.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'LoginTitle' => 'Login Page',
+  'Box1' => 'Enter your Email',
+  'Box2' => 'Enter your password',
+  'Submit' => 'Login',
+);
---
+++ english.php
@@ -0,0 +1,8 @@
+<?php
+
+return array (
+  'LoginTitle' => 'Login Page',
+  'Box1' => 'Enter your Email',
+  'Box2' => 'Enter your password',
+  'Submit' => 'Login',
+);
